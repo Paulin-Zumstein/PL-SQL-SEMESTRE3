060f281141238c0f5339aad923c4d8e1680f0f32
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-10-18 11:08:01 +0200

    question 13 a vérifier

--- PAULIN/BDtp1.sql
+++ PAULIN/BDtp1.sql
@@ -227,7 +227,7 @@ END IF;
 
 IF (DELETING) THEN
 UPDATE Salaries
-SET nbTotalJourneesTravail=nbTotalJourneesTravail11
+SET nbTotalJourneesTravail=nbTotalJourneesTravail-1
 WHERE codeSalarie=:OLD.codeSalarie;
 END IF;
 
@@ -238,13 +238,121 @@ SHOW ERRORS
 
 /*Question 12*/
 
-CREATE VIEW Affectations AS
-SELECT Salaries.codeSalarie, Salaries.nomSalarie, Salaries.prenomSalarie, Equipes.codeEquipe, E.nomEquipe
+CREATE OR REPLACE VIEW Affectations AS
+SELECT Salaries.codeSalarie, Salaries.nomSalarie, Salaries.prenomSalarie, Equipes.codeEquipe, Equipes.nomEquipe
 FROM Salaries
 JOIN EtreAffecte ON Salaries.codeSalarie=EtreAffecte.codeSalarie
 Join Equipes ON Equipes.codeEquipe=Equipes.codeEquipe;
 
 
+
+/*Question 13*/
+
+
+/*solution(solution uniquement du premiers point du 13) si l'insertion dans la vue ne créé ps d'insertion dans EtreAffecte
+
+CREATE OR REPLACE TRIGGER TriggerInsertAffectations
+INSTEAD OF INSERT ON Affectations
+FOR EACH ROW
+
+DECLARE
+v_nbSalarie NUMBER;
+v_nbSalarieVerif NUMBER;
+v_nbEquipe NUMBER;
+v_nbEquipeVerif NUMBER;
+v_nbAffectation NUMBER;
+
+BEGIN
+
+RAISE_APPLICATION_ERROR(-20001, ''); // fonction si RAISE ne stop pas l'xécution du trigger
+
+SELECT COUNT(*) INTO v_nbSalarie
+FROM Salaries
+WHERE codeSalarie=:NEW.codeSalarie;
+
+SELECT COUNT(*) INTO v_nbEquipe
+FROM Equipes
+WHERE codeEquipe=:NEW.codeEquipe;
+
+SELECT COUNT(*) INTO v_nbAffectation
+FROM EtreAffecte
+WHERE codeEquipe=:NEW.codeEquipe AND codeEquipe=:NEW.codeEquipe;
+
+IF v_nbAffectation=0 THEN
+	IF v_nbEquipe>0 AND v_nbSalarie>0 THEN
+	INSERT INTO EtreAffecte(codeSalarie,codeEquipe)
+	VALUES(:NEW.codeSalarie,:NEW.codeEquipe);
+	ELSE
+		IF v_nbEquipe=0 THEN
+		INSERT INTO Equipes(codeEquipe,nomEquipe,codeSalarieChef)
+		VALUES(:NEW.codeEquipe:NEW.nomEquipe,NULL;
+		END IF;
+
+		IF v_nbSalarie=0 THEN
+		INSERT INTO Salaries(codeSalarie,nomSalarie,prenomSalarie,nbTotalJourneesTravail)
+		VALUES(:NEW.codeSalarie,:NEW.nomSalarie,:NEW.prenomSalarie,0);
+		END IF;
+
+		INSERT INTO EtreAffecte(codeSalarie,codeEquipe)
+		VALUES(:NEW.codeSalarie,:NEW.codeEquipe);
+	END IF;
+END IF
+
+END;
+/
+SHOW ERRORS
+
+Sinon cette solution :*/
+
+
+CREATE OR REPLACE TRIGGER TriggerInsertAffectations
+INSTEAD OF INSERT ON Affectations
+FOR EACH ROW
+
+DECLARE
+v_nbSalarie NUMBER;
+v_nbSalarieVerif NUMBER;
+v_nbEquipe NUMBER;
+v_nbEquipeVerif NUMBER;
+v_nbAffectation NUMBER;
+
+BEGIN
+
+
+/*SOLUTION COMPLETE MAIS A VERIFIER*/
+SELECT COUNT(*) INTO v_nbSalarie
+FROM Salaries
+WHERE codeSalarie=:NEW.codeSalarie AND nomSalarie=:NEW.nomSalarie AND prenomSalarie=:NEW.prenomSalarie ;
+
+SELECT COUNT(*) INTO v_nbSalarieVerif
+FROM Salaries
+WHERE codeSalarie=:NEW.codeSalarie;
+
+SELECT COUNT(*) INTO v_nbEquipe
+FROM Equipes
+WHERE codeEquipe=:NEW.codeEquipe AND nomEquipe=:NEW.nomEquipe;
+
+SELECT COUNT(*) INTO v_nbEquipe
+FROM Equipes
+WHERE codeEquipe=:NEW.codeEquipe;
+
+IF v_nbSalarie=v_nbSalarieVerif AND v_nbEquipe=v_nbEquipeVerif THEN
+	IF v_nbSalarie=0 THEN
+	INSERT INTO Salaries(codeSalarie,nomSalarie,prenomSalarie,nbTotalJourneesTravail)
+	VALUES(:NEW.codeSalarie,:NEW.nomSalarie,:NEW.prenomSalarie,0);
+	END IF;
+
+	IF v_nbEquipe=0 THEN
+	INSERT INTO Equipes(codeEquipe,nomEquipe,codeSalarieChef)
+	VALUES(:NEW.codeEquipe,:NEW.nomEquipe,NULL;
+	END IF;
+ELSE 
+RAISE_APPLICATION_ERROR(-20001, 'les attributs ne sont pas bon'); 
+END IF;
+
+END;
+/
+SHOW ERRORS
 
 
 
