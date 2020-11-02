26ac80f45e3422bf9bbad51fe3f68fa1bb722f73
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-11-02 16:56:54 +0100

    question 15 optimisation fini

--- PAULIN/BDoptimisation.sql
+++ PAULIN/BDoptimisation.sql
@@ -30,6 +30,12 @@ COMMIT;
 
 
 
+
+SET TIMING ON;
+SET AUTOTRACE ON;
+
+
+
 /*qustion 2.a*/
 
 SELECT sexeClient, villeClient
@@ -66,13 +72,12 @@ CREATE INDEX idx_Commandes_montantCommande ON Commandes (montantCommande)
 UPDATE Commandes
 SET montantCommande=montantCommande-10
 
+
 --Questin 6
 
 SELECT *
 FROM Commandes
 WHERE montantCommande>3500*3
-
-?
 
 
 --Question 7
@@ -100,7 +105,7 @@ WHERE villeClient='Montpellier'
 
 --Question 8
 
-SELECT /*+ INDEX(Commandes idx_Commandes_dateIdCommande) */  *
+SELECT /*+ INDEX(Commandes idx_Commandes_dateIdCommande) */  * -- * empeche utilisation de l'index
 FROM Commandes
 WHERE dateCommande IS NULL
 
@@ -183,11 +188,100 @@ FROM Commandes
 GROUP BY Commandes.idCommande, dateCommande
 HAVING COUNT(DISTINCT idProduit)=(
 	SELECT COUNT(idProduit)
-	FROM Produits)
+	FROM Produits);
 
 SELECT idCommande, dateCommande
 FROM Commandes c
 WHERE NOT EXISTS(
 	SELECT idProduit FROM Produits
 	MINUS
-	SELECT idProduit FROM LignesCommande WHERE c.idCommande=LignesCommande.idCommande)
+	SELECT idProduit FROM LignesCommande WHERE c.idCommande=LignesCommande.idCommande);
+
+
+--Question 13
+
+-- LA VUE MATERIALIZÉ N'EST PLUS UTILISÉ CAR ELLE N'EST A JOUR
+SELECT COUNT(*)
+FROM Clients 
+	JOIN Commandes ON Clients.idClient=Commandes.idClient
+WHERE nomClient='Palleja' and prenomClient='Xavier'
+
+
+CREATE MATERIALIZED VIEW ClientsCA 
+ENABLE QUERY REWRITE
+AS SELECT Clients.idClient, nomClient, prenomClient,COUNT(*), sum(montantCommande)
+FROM Commandes 
+	JOIN Clients ON Clients.idClient = Commandes.idClient
+GROUP BY Clients.idClient, Clients.nomClient, Clients.prenomClient ;
+
+
+INSERT INTO Commandes (idCommande, dateCommande, idClient, montantCommande)
+VALUES (100001, '01/02/2014', 10001, 0);
+COMMIT;
+
+
+--Question 14
+
+DROP MATERIALIZED VIEW ClientsCA
+
+CREATE MATERIALIZED VIEW LOG ON Clients WITH SEQUENCE
+,ROWID(idClient, nomClient, prenomClient) INCLUDING NEW VALUES;
+CREATE MATERIALIZED VIEW LOG ON Commandes WITH SEQUENCE
+,ROWID(idClient, montantCommande) INCLUDING NEW VALUES;
+
+CREATE MATERIALIZED VIEW ClientsCA 
+REFRESH FAST ON COMMIT
+ENABLE QUERY REWRITE
+AS SELECT Clients.idClient, nomClient, prenomClient,COUNT(*), sum(montantCommande)
+FROM Commandes 
+	JOIN Clients ON Clients.idClient = Commandes.idClient
+GROUP BY Clients.idClient, Clients.nomClient, Clients.prenomClient ;
+
+
+--Question 15
+
+EXECUTE DBMS_STATS.GATHER_SCHEMA_STATS('zumsteinp'); --Calcul des statistique
+
+
+--Afficher quelques statistiques de la table Commandes
+SELECT COLUMN_NAME, NUM_DISTINCT, NUM_NULLS, DENSITY,
+LAST_ANALYZED
+FROM USER_TAB_COLUMNS
+WHERE TABLE_NAME = 'COMMANDES';
+
+--d/   vérification de l'utilisation de l'index de la 8d
+SELECT  idCommande
+FROM Commandes
+WHERE dateCommande IS NULL
+
+--e/ 
+CREATE INDEX idx_Commandes_etatCommande ON Commandes (etatCommande)
+
+--f/
+EXECUTE DBMS_STATS.GATHER_SCHEMA_STATS('zumsteinp');
+
+--g/
+SET AUTOTRACE TRACEONLY
+
+--h/
+SELECT idCommande --INDEX UTILISÉ CAR PLUS RAPIDE AVEC L'INDEX /*+ INDEX(Commandes idx_Commandes_etatCommande) */
+FROM Commandes
+WHERE etatCommande='Payée Liquide'
+
+--i/
+SELECT idCommande --INDEX UTILISÉ CAR PAS RENTABLE  ENVIRON 5%  /*+ INDEX(Commandes idx_Commandes_etatCommande) */
+FROM Commandes
+WHERE etatCommande='Payée CB'
+
+SELECT idCommande --INDEX PAS UTILISÉ CAR PAS RENTABLE  >5%  /*+ INDEX(Commandes idx_Commandes_etatCommande) */
+FROM Commandes
+WHERE etatCommande='Passée'
+
+--J/
+SELECT idCommande 
+FROM Commandes
+WHERE etatCommande <> 'Payée CB'
+
+SELECT idCommande 
+FROM Commandes
+WHERE etatCommande='Payée Liquide' and etatCommande='Passée'
